8bf5eebb9ce28cc20ed77bb1c174768781a74aee
Hasan Alperen SELÇUK <78741582+alperen-selcuk@users.noreply.github.com>
2024-04-13 01:23:30 +0300

    Create firewall.tf

---
+++ firewall.tf
@@ -0,0 +1,34 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "SSH"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "ssh access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "www access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+
+}
